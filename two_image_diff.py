d76df036a0c403dfd308efdbc931d3a111649e38
jyoshida <junya.yoshida.e5@tohoku.ac.jp>
2024-07-22 13:01:06 +0900

    ch2/ch0の演算をするようにした

--- two_image_diff.py
+++ two_image_diff.py
@@ -10,25 +10,27 @@ if __name__ == '__main__':
     filename1 = 'Image_2.txt'
     filename2 = 'Image_3.txt'
 
-    brightness_grid1 = fileio.read_file(filename1)
-    brightness_grid2 = fileio.read_file(filename2)
+    list_array_brightness1, x_y_range1 = fileio.read_file(filename1)
+    list_array_brightness2, x_y_range2 = fileio.read_file(filename2)
+
+    brightness1 = list_array_brightness1[2] / list_array_brightness1[0]
+    brightness2 = list_array_brightness2[2] / list_array_brightness2[0]
 
     # shift brightness_grid2 by (delta_x, delta_y)
-    for delta_x in range(-5, 6):
-        for delta_y in range(-5, 6):
-            
-            brightness_grid2_shift = copy.deepcopy(brightness_grid2)
-            brightness_grid2_shift = np.roll(brightness_grid2_shift, delta_x, axis=0)
-            brightness_grid2_shift = np.roll(brightness_grid2_shift, delta_y, axis=1)
+    for delta_x in range(-2, 3):
+        for delta_y in range(-2, 3):
+            brightness2_shift = copy.deepcopy(brightness2)
+            brightness2_shift = np.roll(brightness2_shift, delta_x, axis=0)
+            brightness2_shift = np.roll(brightness2_shift, delta_y, axis=1)
 
-            diff_image = brightness_grid1 - brightness_grid2_shift
+            diff_image = brightness1 - brightness2_shift
 
             # Plot the image
             fig = plt.figure(figsize=(15, 5))
             ax = fig.add_subplot(1, 3, 1)
             divider = mpl_toolkits.axes_grid1.make_axes_locatable(ax)
             cax = divider.append_axes('right', '5%', pad='3%')
-            im = ax.imshow(brightness_grid1.T, origin='lower')
+            im = ax.imshow(brightness1, origin='lower')
             fig.colorbar(im, cax=cax)
             ax.set_title('Image_initial')
             ax.set_xlabel('X')
@@ -37,7 +39,7 @@ if __name__ == '__main__':
             ax = fig.add_subplot(1, 3, 2)
             divider = mpl_toolkits.axes_grid1.make_axes_locatable(ax)
             cax = divider.append_axes('right', '5%', pad='3%')
-            im = ax.imshow(brightness_grid2.T, origin='lower')
+            im = ax.imshow(brightness2, origin='lower')
             fig.colorbar(im, cax=cax)
             ax.set_title('Image_final')
             ax.set_xlabel('X')
@@ -46,7 +48,7 @@ if __name__ == '__main__':
             ax = fig.add_subplot(1, 3, 3)
             divider = mpl_toolkits.axes_grid1.make_axes_locatable(ax)
             cax = divider.append_axes('right', '5%', pad='3%')
-            im = ax.imshow(diff_image.T, origin='lower')
+            im = ax.imshow(diff_image, origin='lower')
             fig.colorbar(im, cax=cax)
             ax.set_title('Difference')
             ax.set_xlabel('X')
@@ -55,5 +57,3 @@ if __name__ == '__main__':
             plt.tight_layout()
             #plt.show()
             plt.savefig(f'diff_image_x{delta_x:+}_y{delta_y:+}.png')
-
-
